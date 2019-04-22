5fc6e29c93c086e59f65ab4da5918dbf225b5271
pat-s <patrick.schratz@gmail.com>
2019-04-22 23:58:29 +0200

    add more dots

---
+++ .Rprofile.d/repos.R
@@ -0,0 +1,4 @@
+# options(repos=structure(c(CRAN="https://mirrors.cicku.me/CRAN/")))
+
+options(repos = structure(c(CRAN = "https://cran.uni-muenster.de/", 
+                            BioCsoft = "https://bioconductor.org/packages/3.7/bioc")))
