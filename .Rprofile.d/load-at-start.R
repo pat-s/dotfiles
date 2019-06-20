375727e6621df2752ea4146fa35497a23bdf0c97
pat-s <patrick.schratz@gmail.com>
2019-06-20 22:56:43 +0200

    .Rprofile.d

---
+++ .Rprofile.d/load-at-start.R
@@ -0,0 +1,9 @@
+#library("mapview", quietly = TRUE)
+#library("crancache")
+#library("usethis")
+if (interactive()) {
+  suppressMessages(require(usethis))
+}
+library("colorout")
+#library("rcourses")
+library("gert")
