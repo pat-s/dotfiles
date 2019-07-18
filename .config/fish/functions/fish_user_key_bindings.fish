f036a6dd4a89b542b731c1ec8f1eebbd1f8f9698
pat-s <patrick.schratz@gmail.com>
2019-07-18 09:26:20 +0200

    add todoist binding

---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,8 @@
+function fish_user_key_bindings
+  fish_vi_key_bindings
+  bind \ctt peco_todoist_item
+  bind \ctp peco_todoist_project
+  bind \ctl peco_todoist_labels
+  bind \ctc peco_todoist_close
+  bind \ctd peco_todoist_delete
+end
