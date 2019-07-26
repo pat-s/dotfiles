140aef3fa11df14b5b0bdbd627723ad64acc3ce2
pat-s <patrick.schratz@gmail.com>
2019-07-26 09:48:31 +0200

    use new R mirror

--- .Rprofile.d/repos.R
+++ .Rprofile.d/repos.R
@@ -1,4 +1,4 @@
 # options(repos=structure(c(CRAN="https://mirrors.cicku.me/CRAN/")))
 
-options(repos = structure(c(CRAN = "https://cran.uni-muenster.de/", 
+options(repos = structure(c(CRAN = "https://ftp.fau.de/cran",
                             BioCsoft = "https://bioconductor.org/packages/3.7/bioc")))
