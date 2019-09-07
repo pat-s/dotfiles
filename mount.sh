5cdf00757edca039c3d7c7f7044968df7790a86d
pat-s <patrick.schratz@gmail.com>
2019-09-08 00:37:51 +0100

    mount scripts

--- mount.sh
+++ mount.sh
@@ -1,14 +1,11 @@
-# mccoy
-sudo sshfs -o reconnect,idmap=user,transform_symlinks,identityFile=~/.ssh/id_rsa,allow_other,cache=yes,kernel_cache,compression=no,default_permissions,uid=1000,gid=100,umask=0 patrick@141.35.159.150:/ /mnt/mccoy
+# unstrut
+sudo mount -t cifs -o credentials=/home/pjs/.credentials,uid=1000,file_mode=0775,dir_mode=0775,gid=100,sec=ntlm,vers=1.0,dom=ads.uni-jena.de,forcegid //141.35.159.70/home_geoinf     /mnt/geoinf_home
 
 # unstrut
-sudo mount -t cifs -o credentials=/home/pjs/.smbcredentials.txt,uid=1000,file_mode=0775,dir_mode=0775,gid=100,sec=ntlm,vers=1.0,dom=ads.uni-jena.de,forcegid //141.35.159.70/home_geoinf     /mnt/geoinf_home
-
-# unstrut
-sudo mount -t cifs -o credentials=/home/pjs/.smbcredentials.txt,uid=1000,file_mode=0775,dir_mode=0775,gid=100,sec=ntlm,vers=1.0,dom=ads.uni-jena.de,forcegid //141.35.159.70/home_web     /mnt/geoinf_web
+sudo mount -t cifs -o credentials=/home/pjs/.credentials,uid=1000,file_mode=0775,dir_mode=0775,gid=100,sec=ntlm,vers=1.0,dom=ads.uni-jena.de,forcegid //141.35.159.70/home_web     /mnt/geoinf_web
 
 # lossa
-sudo mount -t cifs -o credentials=/home/pjs/.smbcredentials.txt,uid=1000,file_mode=0775,dir_mode=0775,gid=100,sec=ntlm,vers=1.0,dom=ads.uni-jena.de,forcegid //141.35.159.90/data     /mnt/lossa
+sudo mount -t cifs -o credentials=/home/pjs/.credentials,uid=1000,file_mode=0775,dir_mode=0775,gid=100,sec=ntlm,vers=1.0,dom=ads.uni-jena.de,forcegid //141.35.159.90/data     /mnt/lossa
 
 # venus
 sudo sshfs -o reconnect,idmap=user,transform_symlinks,identityFile=~/.ssh/id_rsa,allow_other,cache=yes,kernel_cache,compression=no,default_permissions,uid=1000,gid=100,umask=0 patrick@141.35.159.80:/ /mnt/venus
--- mymount.sh
+++ mymount.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
 sudo pkill -kill -f "sshfs"
-sudo umount -f /mnt/mccoy
-sudo umount -l /mnt/mccoy
 sudo umount -f /mnt/venus
-sudo umount -l /mnt/venus
-sudo umount -a -t cifs -l
-sudo bash /home/pjs/mount.sh
+sudo umount -a -t cifs
+sudo bash ~/mount.sh
