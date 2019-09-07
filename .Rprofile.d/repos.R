8ddc6644d2e765426cccf19b30129b1852ad844c
pat-s <patrick.schratz@gmail.com>
2019-09-08 00:36:37 +0100

    use new cran mirror

--- .Rprofile.d/repos.R
+++ .Rprofile.d/repos.R
@@ -1,4 +1,6 @@
 # options(repos=structure(c(CRAN="https://mirrors.cicku.me/CRAN/")))
 
-options(repos = structure(c(CRAN = "https://ftp.fau.de/cran",
-                            BioCsoft = "https://bioconductor.org/packages/3.7/bioc")))
+options(repos = structure(c(CRAN = "https://ftp.fau.de/cran"#,
+#                            BioCsoft = "https://bioconductor.org/packages/3.7/bioc"
+)
+))
