262f72fc9eea50c6dd0048222a6dc22a100d1762
pat-s <patrick.schratz@gmail.com>
2019-10-31 17:33:42 +0100

    rstudio theme

---
+++ .Rprofile.d/rstudio-theme.R
@@ -0,0 +1,14 @@
+if (
+  interactive() &&
+  requireNamespace("rsthemes", quietly = TRUE) &&
+  requireNamespace("later", quietly = TRUE)
+) {
+  # Use later to delay until RStudio is ready
+  later::later(function() {
+    rsthemes::set_theme_light("One Light {rsthemes}")  # light theme
+    rsthemes::set_theme_dark("Mojave Dark") # dark theme
+
+    # To automatically choose theme based on time of day
+    rsthemes::use_theme_auto(dark_start = "18:00", dark_end = "6:00")
+  }, delay = 1)
+}
