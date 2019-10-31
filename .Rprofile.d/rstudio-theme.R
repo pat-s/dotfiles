2be622e1f3ba604ada74706f47cf96d6d4c8942e
pat-s <patrick.schratz@gmail.com>
2019-10-31 17:39:39 +0100

    optimize rstudio theme times

--- .Rprofile.d/rstudio-theme.R
+++ .Rprofile.d/rstudio-theme.R
@@ -9,6 +9,6 @@ if (
     rsthemes::set_theme_dark("Mojave Dark (Static)") # dark theme
 
     # To automatically choose theme based on time of day
-    rsthemes::use_theme_auto(dark_start = "18:00", dark_end = "6:00")
+    rsthemes::use_theme_auto(dark_start = "17:00", dark_end = "8:00")
   }, delay = 1)
 }
