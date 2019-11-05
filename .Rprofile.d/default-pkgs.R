e2dde4c52b446b8d8a524734402546e7ee29c8e4
pat-s <patrick.schratz@gmail.com>
2019-11-05 23:51:20 +0100

    load quietly

--- .Rprofile.d/default-pkgs.R
+++ .Rprofile.d/default-pkgs.R
@@ -1,6 +1,6 @@
 
-if (!requireNamespace("remotes")) install.packages("remotes")
-if (!requireNamespace("fnmate")) remotes::install_github("MilesMcBain/fnmate")
-if (!requireNamespace("gistfo")) remotes::install_github("MilesMcBain/gistfo")
-if (!requireNamespace("packup")) remotes::install_github("MilesMcBain/packup")
-if (!requireNamespace("styler")) remotes::install_github("pat-s/styler@mlr-style")
+if (!requireNamespace("remotes", quietly = TRUE)) install.packages("remotes")
+if (!requireNamespace("fnmate", quietly = TRUE)) remotes::install_github("MilesMcBain/fnmate")
+if (!requireNamespace("gistfo", quietly = TRUE)) remotes::install_github("MilesMcBain/gistfo")
+if (!requireNamespace("packup", quietly = TRUE)) remotes::install_github("MilesMcBain/packup")
+if (!requireNamespace("styler", quietly = TRUE)) remotes::install_github("pat-s/styler@mlr-style")
