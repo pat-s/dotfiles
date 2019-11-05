2c1b9fe7f87bc57e53642aeddeb03c5d8e76cc08
pat-s <patrick.schratz@gmail.com>
2019-11-05 14:02:05 +0100

    add default-pkgs.R

---
+++ .Rprofile.d/default-pkgs.R
@@ -0,0 +1,6 @@
+
+if (!requireNamespace("remotes")) install.packages("remotes")
+if (!requireNamespace("fnmate")) remotes::install_github("MilesMcBain/fnmate")
+if (!requireNamespace("gistfo")) remotes::install_github("MilesMcBain/gistfo")
+if (!requireNamespace("packup")) remotes::install_github("MilesMcBain/packup")
+if (!requireNamespace("styler")) remotes::install_github("pat-s/styler@mlr-style")
