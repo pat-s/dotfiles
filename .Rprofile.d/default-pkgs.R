329b5c83799782fcfb86e61e8366cabda99bcb1a
pat-s <patrick.schratz@gmail.com>
2019-11-05 14:03:53 +0100

    default pkgs for R

--- .Rprofile.d/default-pkgs.R
+++ .Rprofile.d/default-pkgs.R
@@ -1,6 +1,9 @@
 
 if (!requireNamespace("remotes")) install.packages("remotes")
+if (!requireNamespace("startup")) install.packages("startup")
 if (!requireNamespace("fnmate")) remotes::install_github("MilesMcBain/fnmate")
 if (!requireNamespace("gistfo")) remotes::install_github("MilesMcBain/gistfo")
 if (!requireNamespace("packup")) remotes::install_github("MilesMcBain/packup")
 if (!requireNamespace("styler")) remotes::install_github("pat-s/styler@mlr-style")
+if (!requireNamespace("teamtools")) remotes::install_github("lorenzwalthert/teamtools")
+if (!requireNamespace("usethis")) install.packages("usethis")
