e26ae40edc336bb63e54fa3cb23ade931ed86f40
pat-s <patrick.schratz@gmail.com>
2019-12-01 21:07:04 +0100

    various

--- .Rprofile.d/repos.R
+++ .Rprofile.d/repos.R
@@ -1,7 +1,4 @@
-# options(repos=structure(c(CRAN="https://mirrors.cicku.me/CRAN/")))
-
 options(repos = structure(c(CRAN = "https://ftp.fau.de/cran"#,
-#                            BioCsoft = "https://bioconductor.org/packages/3.7/bioc"
 )))
-options(repos = BiocManager::repositories())
+#options(repos = BiocManager::repositories())
 
