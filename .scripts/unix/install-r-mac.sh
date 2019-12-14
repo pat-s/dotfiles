264d9d7c54bb08d7bd1822abbbda3a8e62116239
pat-s <patrick.schratz@gmail.com>
2019-12-14 09:31:13 +0100

    install r-macos

---
+++ .scripts/unix/install-r-mac.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+R_VERSION=$1
+
+wget https://mac.r-project.org/el-capitan/R-${R_VERSION}-branch/R-${R_VERSION}-branch-el-capitan-sa-x86_64.tar.gz
+
+sudo tar fvxz R*.tar.gz -C /
+
+rm R*.tar.gz
