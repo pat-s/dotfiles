eb02fb53adc4c6f4602682ae01c0258701e6f496
pat-s <patrick.schratz@gmail.com>
2020-02-15 10:00:19 +0100

    test

---
+++ .Renviron.d/envs.R
@@ -0,0 +1,16 @@
+## Supported since R (>= 3.4.0)
+_R_CHECK_LENGTH_1_CONDITION_=true
+
+## Supported since R (>= 3.6.0)
+_R_CHECK_LENGTH_1_LOGIC2_=true
+R_REMOTES_UPGRADE="always"
+
+#R_TRAVIS = ".com"
+R_TRAVIS_ORG="mW7VerLvZABp2E0S8J-K-Q"
+R_TRAVIS_COM="-FPc8fd1C6lx7sCK_foCTQ"
+
+R_CIRCLE="328602db6cabe164cfe1b0273c681e2393c6892f"
+
+# don't error if a package is not available for a specific R version
+R_REMOTES_NO_ERRORS_FROM_WARNINGS = "true"
+
