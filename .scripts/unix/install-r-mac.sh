a3ffd11d72817bdd2630e155f2d983d7aa077818
pat-s <patrick.schratz@gmail.com>
2020-02-22 16:26:11 +0100

    use curl instead of wget

--- .scripts/unix/install-r-mac.sh
+++ .scripts/unix/install-r-mac.sh
@@ -2,7 +2,7 @@
 
 R_VERSION=$1
 
-wget https://mac.r-project.org/el-capitan/R-${R_VERSION}-branch/R-${R_VERSION}-branch-el-capitan-sa-x86_64.tar.gz
+curl -O https://mac.r-project.org/el-capitan/R-${R_VERSION}-branch/R-${R_VERSION}-branch-el-capitan-sa-x86_64.tar.gz
 
 sudo tar fvxz R*.tar.gz -C /
 
