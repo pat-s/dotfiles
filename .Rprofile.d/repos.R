6c6d572c6c32a980b3a8cfef1a797b17f368dc30
pat-s <patrick.schratz@gmail.com>
2021-04-27 18:52:33 +0200

    upd

--- .Rprofile.d/repos.R
+++ .Rprofile.d/repos.R
@@ -1,6 +1,8 @@
-options(repos = structure(c(
-                            CRAN = "https://stat.ethz.ch/CRAN/"#,
-                            #mlr3learners = 'https://mlr3learners.github.io/mlr3learners.drat'
-)))
+if (Sys.info()[["sysname"]] == "Darwin") {
+  options(repos = structure(c(
+                              CRAN = "https://stat.ethz.ch/CRAN/"#,
+                              #mlr3learners = 'https://mlr3learners.github.io/mlr3learners.drat'
+  )))
+}
 #options(repos = BiocManager::repositories())
 
