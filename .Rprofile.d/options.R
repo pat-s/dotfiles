7838a1deb512e26868032065a59d73c6367ba605
pat-s <patrick.schratz@gmail.com>
2021-06-04 17:06:45 +0200

    update dotfiles

--- .Rprofile.d/options.R
+++ .Rprofile.d/options.R
@@ -1,45 +1,46 @@
-options(scipen = 999) # do not print e-10 for small numbers
 options(
   usethis.full_name = "Patrick Schratz",
-  usethis.protocol  = "ssh",
+  usethis.protocol = "ssh",
   usethis.description = list(
     `Authors@R` = 'person("Patrick", "Schratz", email = "patrick.schratz@gmail.com", role = c("aut", "cre"),
     comment = c(ORCID = "0000-0003-0748-6624"))',
     Version = "0.0.0.9000"
-  )
+  ),
+  startup.check.options.ignore = "stringsAsFactors",
+  Ncpus = 10L,
+  reprex.style = TRUE,
+  # suppress rmarkdown vignettes warnings
+  rmarkdown.html_vignette.check_title = FALSE,
+  startup.check.options.ignore = "error",
+  # do not print e-10 for small numbers
+  scipen = 999
 )
-options(reprex.style = TRUE) # use styler indention when doing a reprex
+if (Sys.info()["sysname"] == "Darwin") {
+  options(
+    Ncpus = 10L,
+    install.packages.check.source = "no",
+    pkgType = "source",
+    precommit.executable = "/usr/local/bin/pre-commit",
+    precommit.path_cp_config_from = "https://raw.githubusercontent.com/mlr-org/mlr/master/.pre-commit-config.yaml"
+  )
+}
 
 if (grepl("mlr", getwd()) || grepl("paradox", getwd())) {
   options(styler.addins_style_transformer = "styler.mlr::mlr_style()")
 }
 
 # tibble > data.frame
-#if (interactive() && "tibble" %in% rownames(utils::installed.packages())) {
+# if (interactive() && "tibble" %in% rownames(utils::installed.packages())) {
 #  print.data.frame = function(x, ...) {
 #    tibble:::print.tbl_df(tibble::as_tibble(x), ...)
 #  }
-#}
-
-# suppress rmarkdown vignettes warnings
-options(rmarkdown.html_vignette.check_title = FALSE)
-
-options(
-    Ncpus = 10L,
-    usethis.protocol = "ssh",
-    install.packages.check.source = "no",
-    pkgType = "source",
-    precommit.executable = "/usr/local/bin/pre-commit",
-    precommit.path_cp_config_from = "https://raw.githubusercontent.com/mlr-org/mlr/master/.pre-commit-config.yaml",
-    startup.check.options.ignore = "stringsAsFactors"
-)
+# }
 
 if (as.numeric(version$major) >= 4) {
   options(stringsAsFactors = FALSE)
   Sys.setenv("R_DEV" = TRUE)
 }
 
-options(startup.check.options.ignore = "error")
 # from https://renkun.me/2020/03/31/a-simple-way-to-show-stack-trace-on-error-in-r/
 options(error = function() {
   calls <- sys.calls()
@@ -56,4 +57,3 @@ options(error = function() {
     q(status = 1)
   }
 })
-
