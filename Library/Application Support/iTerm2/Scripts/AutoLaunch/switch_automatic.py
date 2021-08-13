d7acf34e3afc056b5f5e264d28dd133733e7a956
pat-s <patrick.schratz@gmail.com>
2021-08-13 22:10:33 +0200

    update dotfiles

--- Library/Application Support/iTerm2/Scripts/AutoLaunch/switch_automatic.py
+++ Library/Application Support/iTerm2/Scripts/AutoLaunch/switch_automatic.py
@@ -3,24 +3,62 @@
 import asyncio
 import iterm2
 
+THEME_LIGHT = "Tomorrow"
+THEME_DARK = "Smoooooth"
+
+
+class AutoSwtichTheme:
+    def __init__(self, connection, light="Light Background", dark="Dark Background"):
+        self.connection = connection
+        self.light = light
+        self.dark = dark
+
+    async def get_app(self):
+        return await iterm2.async_get_app(self.connection)
+
+    async def get_theme(self) -> str:
+        parts = await (await self.get_app()).async_get_theme()
+        if len(parts) <= 1:
+            return parts[0]
+        return ""
+
+    async def set_color_preset(self, theme):
+        preset = await iterm2.ColorPreset.async_get(
+            self.connection, self.light if theme == "light" else self.dark
+        )
+
+        profiles = await iterm2.PartialProfile.async_query(self.connection)
+        for partial in profiles:
+            await (await partial.async_get_full_profile()).async_set_color_preset(
+                preset
+            )
+
+
+async def quit(connection):
+    while True:
+        if not connection.websocket.open:
+            exit(0)
+        await asyncio.sleep(1)
+
+
 async def main(connection):
-    async with iterm2.VariableMonitor(connection, iterm2.VariableScopes.APP, "effectiveTheme", None) as mon:
+    asyncio.ensure_future(quit(connection), loop=asyncio.get_event_loop())
+
+    ast = AutoSwtichTheme(connection, THEME_LIGHT, THEME_DARK)
+    await ast.set_color_preset(await ast.get_theme())
+
+    async with iterm2.VariableMonitor(
+        connection, iterm2.VariableScopes.APP, "effectiveTheme", None
+    ) as mon:
         while True:
             # Block until theme changes
             theme = await mon.async_get()
 
-            # Themes have space-delimited attributes, one of which will be light or dark.
-            parts = theme.split(" ")
-            if "dark" in parts:
-                preset = await iterm2.ColorPreset.async_get(connection, "Smoooooth")
-            else:
-                preset = await iterm2.ColorPreset.async_get(connection, "Tomorrow")
+            # Set preset if theme has changed
+            await ast.set_color_preset(theme)
 
-            # Update the list of all profiles and iterate over them.
-            profiles=await iterm2.PartialProfile.async_query(connection)
-            for partial in profiles:
-                # Fetch the full profile and then set the color preset in it.
-                profile = await partial.async_get_full_profile()
-                await profile.async_set_color_preset(preset)
 
-iterm2.run_forever(main)
+try:
+    iterm2.run_forever(main)
+except:
+    print("Unable to connect on iTerm2 application")
