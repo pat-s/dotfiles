5e43329586865edbe533bd9c0645ac46061ddf15
pat-s <patrick.schratz@gmail.com>
2021-09-07 10:59:56 +0200

    update dotfiles

--- .Rprofile.d/repos.R
+++ .Rprofile.d/repos.R
@@ -1,8 +1,10 @@
 if (Sys.info()[["sysname"]] == "Darwin") {
   options(repos = structure(c(
-                              CRAN = "https://stat.ethz.ch/CRAN/"#,
-                              #mlr3learners = 'https://mlr3learners.github.io/mlr3learners.drat'
+    CRAN = "https://stat.ethz.ch/CRAN/" # ,
+    # mlr3learners = 'https://mlr3learners.github.io/mlr3learners.drat'
+  )))
+} else if (Sys.info()[["nodename"]] == "parallels") {
+  options(repos = structure(c(
+    CRAN = "https://packagemanager.rstudio.com/all/latest"
   )))
 }
-#options(repos = BiocManager::repositories())
-
