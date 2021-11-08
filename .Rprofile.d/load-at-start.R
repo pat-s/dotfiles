5f44bd92ce86b6376abd0d1f04b8c088ed1c8ca6
pat-s <patrick.schratz@gmail.com>
2021-11-08 14:09:29 +0100

    update dotfiles

--- .Rprofile.d/load-at-start.R
+++ .Rprofile.d/load-at-start.R
@@ -5,19 +5,17 @@ if (interactive() && Sys.info()[["sysname"]] == "Darwin") {
   suppressMessages(require(reprex))
   suppressMessages(require(fledge))
   suppressMessages(require(remotes))
-  suppressMessages(require(prettycode))
+  #suppressMessages(require(prettycode))
   suppressMessages(require(browse))
 }
 #if (requireNamespace("prompt", quietly = TRUE)) {
 #  prompt::set_prompt(prompt::prompt_git)
 #}
 
-# remotes::install_github("krlmlr/fledge")
-# remotes::install_github("pat-s/usethis@my-prs")
+# remotes::install_github("cynkra/fledge")
 # remotes::install_github("r-lib/prettycode")
 # remotes::install_github("tmastny/browse")
 # remotes::install_github("lorenzwalthert/teamtools")
-# remotes::install_github("lorenzwalthert/styler@mlr-style")
 # remotes::install_github("gadenbuie/rsthemes")
 # remotes::install_github("mine-cetinkaya-rundel/addmins")
 # install.packages(c("reprex", "remotes", "startup"))
